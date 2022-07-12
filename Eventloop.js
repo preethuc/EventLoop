f102b630948dec03c7baf9a0f9d6980e7b379f44
preetha <preetha@sparkouttech.com>
2022-07-12 10:19:32 +0530

    first commit

---
+++ Eventloop.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const crypto = require("crypto");
+start = Date.now();
+setImmediate(() => console.log("Timer 1 set "));
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setTimeout(() => console.log("Timer 2 finished"), 0);
+//console.log("First Line");
+fs.readFile("test-file.txt", () => {
+  console.log("i/o finished");
+  console.log("============");
+  setTimeout(() => console.log("Timer 3 finished"), 0);
+  setTimeout(() => console.log("Timer 4 finished"), 0);
+  setImmediate(() => console.log("Timer 2 set "));
+  process.nextTick(() => console.log("Process of nexttick"));
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted 1");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted 2");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted 3");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted 4");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted 5");
+  });
+});
+console.log("First Line");
